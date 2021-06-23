f579dda91ccc7354a8fc028e3290eca30c93bb29
vilnytskyy <vilnytskyy21@gmail.com>
2021-06-22 23:20:31 -0400

    Added addRow func

--- script.js
+++ script.js
@@ -1,38 +1,68 @@
 let numRows = 0;
 let numCols = 0;
-let colorSelected; 
+let colorSelected;
+let grid = document.getElementById("grid");
 
-//Adds a row
-function addR() {
-    alert("Clicked Add Row")
+// Adds a row
+function addRow() {
+    if (numCols === 0) {
+        numCols++;
+    }
+
+    const row = document.createElement("tr");
+    const col = document.createElement("td");
+    row.appendChild(col);
+    grid.appendChild(row);
+
+    numRows++;
 }
-//Adds a column
-function addC() {
+
+
+
+// Adds a column
+function addCol() {
     alert("Clicked Add Col")
 }
 
-//Removes a row
-function removeR() {
+
+
+// Removes a row
+function removeRow() {
     alert("Clicked Remove Row")
 }
-//Remove a column
-function removeC() {
+
+
+
+// Removes a column
+function removeCol() {
     alert("Clicked Remove Col")
 }
-//sets global var for selected color
-function selected(){
+
+
+
+// Sets global var to selected color
+function selected() {
     colorSelected = document.getElementById("selectedID").value;
     console.log(colorSelected);
 }
 
-function fill(){
+
+
+// Fills all cells with the selected color
+function fill() {
     alert("Clicked Fill All")
 }
 
-function clearAll(){
+
+
+// Clears all cells
+function clearAll() {
     alert("Clicked Clear All")
 }
 
-function fillU(){
+
+
+// Fills all uncolored cells with the selected color
+function fillUncolored() {
     alert("Clicked Fill All Uncolored")
 }
